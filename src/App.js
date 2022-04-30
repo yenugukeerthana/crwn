2411417e90048bdf2ce98f85be0f09a0cabc7c37
yenugukeerthana <yenugukeerthana@gmail.com>
2022-04-30 12:30:55 +0530

    Add. BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './categories.styles.scss'
+import Directory from './components/directory/directory.component';
 const App=()=>{
   const categories=[
     {
@@ -28,22 +28,8 @@ const App=()=>{
     },
   ];
   return(
-    <div className='categories-container'>
-      {categories.map(({title,id,imageUrl})=>(
-      <div key={id} className='category-container'>
-        <div className='background-image' style={{
-          backgroundImage:`url(${imageUrl})`,
-        }}
-        />
-        <div className='category-body-container'>
-          <h2>{title}</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
-      ))}
-    </div>
+    <Directory key={categories.id} categories={categories}/>
   );
 };
 
 export default App;
-
